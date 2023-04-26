d756b2c01563749010a890e84f77af4c90c3fe83
c21333116 <123654910+c21333116@users.noreply.github.com>
2023-04-26 18:27:44 +0100

    l

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -87,3 +87,51 @@ public class DANI extends PApplet {
 		}
     }
 }
+
+
+class Word {
+    private String word;
+    private ArrayList<follow> fields;
+
+    public Word(String word, ArrayList<follow> fields) {
+        this.word = word;
+        this.fields = fields;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public ArrayList<follow> getFields() {
+        return fields;
+    }
+
+    public String toString() {
+        String result = word + "\n";
+        for (follow follow : fields) {
+            result += "\t" + follow + "\n";
+        }
+        return result;
+    }
+
+    public follow findFollow(String str) {
+        for (follow f : fields) {
+            if (f.getWord().equals(str)) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public void printModel() {
+        for (Word word : words) {
+            System.out.println(word);
+        }
+    }
+}
+
+
+
+
+
+
